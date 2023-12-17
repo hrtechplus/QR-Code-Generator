a45c14e0defc9534e6e4520dc4ef0b415fd9d8ed
hrtechplus <40927446+hrtechplus@users.noreply.github.com>
2023-12-17 18:17:18 +0530

    Update ImgComponent.js

--- client/src/components/ImgComponent.js
+++ client/src/components/ImgComponent.js
@@ -1,5 +1,21 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
-export default function ImgComponent() {
-  return <Box></Box>;
+import { Box, Image, Text } from "@chakra-ui/react";
+
+export default function ImgComponent({ imageUrl, caption }) {
+  return (
+    <Box textAlign="center" maxW="300px" mx="auto">
+      <Image
+        src={imageUrl}
+        alt="Image"
+        boxSize="100%"
+        objectFit="cover"
+        borderRadius="md"
+      />
+      {caption && (
+        <Text mt={2} color="gray.600" fontSize="sm">
+          {caption}
+        </Text>
+      )}
+    </Box>
+  );
 }
