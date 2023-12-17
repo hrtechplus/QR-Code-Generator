d4cb6a2e0df8e03dcfef491a0393016cbcf83dd1
hrtechplus <40927446+hrtechplus@users.noreply.github.com>
2023-12-17 12:56:39 +0530

    Update Main.js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -7,23 +7,33 @@ export default function CenteredBox() {
   return (
     <Center>
       <Box
-        px={[4, 8]} // Responsive padding on the x-axis
-        py={[6, 12]} // Responsive padding on the y-axis
-        boxShadow="xs"
+        px={[2, 4, 8]} // Responsive padding on the x-axis
+        py={[4, 6, 12]} // Responsive padding on the y-axis
+        boxShadow="md"
         rounded="md"
-        maxW="md" // Set maximum width for better mobile responsiveness
+        maxW={["100%", "400px", "600px"]} // Set maximum width for better responsiveness
         w="100%" // Make sure the box takes the full width
       >
-        <Flex direction="column" align="center">
-          <Center w="100%" bg="green.500" mb={4}>
+        <Flex
+          direction={["column", "column", "row"]}
+          align="center"
+          justify={"center"}
+          gap={[4, 4, 0]} // Adjust the gap for spacing on smaller screens
+        >
+          <Center
+            w={["100%", "100%", "20%"]} // Adjust the width for different screen sizes
+            bg="green.500"
+            mb={[4, 4, 0]} // Adjust margin-bottom for spacing on smaller screens
+          >
             <Text>Box 1</Text>
           </Center>
-          <Square bg="blue.500" size={["100px", "150px"]} mb={4}>
+          <Square
+            bg="blue.500"
+            size={["100px", "150px", "150px"]} // Adjust the size for different screen sizes
+            mb={[4, 4, 0]} // Adjust margin-bottom for spacing on smaller screens
+          >
             <Text>Box 2</Text>
           </Square>
-          <Box w="100%" bg="tomato">
-            <Text>Box 3</Text>
-          </Box>
         </Flex>
       </Box>
     </Center>
