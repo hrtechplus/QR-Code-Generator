9ee887e80468e1e1700d637f35eabd792cf0ebfa
hrtechplus <40927446+hrtechplus@users.noreply.github.com>
2023-12-17 23:20:52 +0530

    LinkBtn

---
+++ client/src/components/LinkBtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LinkBtn.css";
+export default function LinkBtn() {
+  return (
+    <div class="container">
+      <div class="toggle">
+        <input type="checkbox" />
+        <span class="button"></span>
+        <span class="label">☼</span>
+      </div>
+    </div>
+  );
+}
