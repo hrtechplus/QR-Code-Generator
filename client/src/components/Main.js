d543f76e7d8facecc58fc04a159e6375087a000d
hrtechplus <40927446+hrtechplus@users.noreply.github.com>
2023-12-17 18:57:32 +0530

    Update Main.js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,41 +1,34 @@
 import React from "react";
-import { Flex, Center, Square, Text, Box, Img } from "@chakra-ui/react";
-import QRImg from "./QRImg";
-import InputField from "./InputField";
+import { Flex, Center, Box, Square } from "@chakra-ui/react";
 import ImgComponent from "./ImgComponent";
 import Form from "./Form";
-import "./Main.css";
 
 export default function CenteredBox() {
   return (
     <Center minH="100vh">
       <Box
         className="MainContainer"
-        px={[2, 4, 8]} // Responsive padding on the x-axis
-        py={[4, 6, 12]} // Responsive padding on the y-axis
+        p={[2, 4, 8]} // Responsive padding
         boxShadow="md"
         rounded="md"
-        maxW={["100%", "400px", "600px"]} // Set maximum width for better responsiveness
-        w="100%" // Make sure the box takes the full width
+        maxW={["100%", "400px", "600px"]} // Set maximum width for responsiveness
+        w="100%" // Take the full width
       >
         <Flex
           direction={["column", "column", "row"]}
-          align="center"
-          justify={"space-around"}
-          gap={[4, 4, 0]} // Adjust the gap for spacing on smaller screens
+          align={["center", "center", "flex-start"]}
+          justify={["center", "center", "space-between"]}
+          gap={[4, 4, 8]} // Adjust the gap for spacing on smaller screens
         >
           <Center
-            w={["100%", "100%", "20%"]} // Adjust the width for different screen sizes
+            w={["100%", "100%", "40%"]} // Adjust the width for different screen sizes
             mb={[4, 4, 0]} // Adjust margin-bottom for spacing on smaller screens
           >
-            {/* // METHANA THAMAI OKKOMA COMPENETS THIENNE */}
-
             <ImgComponent />
           </Center>
           <Square
             bg="blue.500"
-            size={["100px", "150px", "150px"]} // Adjust the size for different screen sizes
-            mb={[4, 4, 0]} // Adjust margin-bottom for spacing on smaller screens
+            size={["100%", "150px", "150px"]} // Adjust the size for different screen sizes
           >
             <Form />
           </Square>
