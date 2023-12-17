084a92a58721353b32c13956b8a70b2e83543724
hrtechplus <40927446+hrtechplus@users.noreply.github.com>
2023-12-17 18:46:41 +0530

    form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,10 +1,146 @@
-import React from "react";
-import InputField from "./InputField";
+import React, { useState } from "react";
+import {
+  Box,
+  VStack,
+  HStack,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Button,
+  RadioGroup,
+  Radio,
+  Textarea,
+} from "@chakra-ui/react";
 
-export default function Form() {
+export default function QRCodeForm() {
+  const [formData, setFormData] = useState({
+    data: "",
+    imageUri: "",
+    innerEye: "circle",
+    outerEye: "circle",
+    backgroundColor: "black",
+    size: 200,
+    outputFormat: "png",
+  });
+
+  const handleChange = (key, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [key]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    // Add your logic to handle form submission
+    console.log(formData);
+  };
+
   return (
-    <div>
-      <InputField />
-    </div>
+    <Box p={4} maxW="500px" mx="auto">
+      <VStack spacing={4}>
+        <Heading as="h1" size="lg" textAlign="center">
+          QR Code Generator
+        </Heading>
+
+        <FormControl isRequired>
+          <FormLabel>Data</FormLabel>
+          <Input
+            type="text"
+            placeholder="Enter data"
+            value={formData.data}
+            onChange={(e) => handleChange("data", e.target.value)}
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Image URI (Optional)</FormLabel>
+          <Input
+            type="text"
+            placeholder="Enter image URI"
+            value={formData.imageUri}
+            onChange={(e) => handleChange("imageUri", e.target.value)}
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Inner Eye</FormLabel>
+          <RadioGroup
+            value={formData.innerEye}
+            onChange={(value) => handleChange("innerEye", value)}
+          >
+            <HStack spacing={4}>
+              <Radio value="circle">Circle</Radio>
+              <Radio value="square">Square</Radio>
+            </HStack>
+          </RadioGroup>
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Outer Eye</FormLabel>
+          <RadioGroup
+            value={formData.outerEye}
+            onChange={(value) => handleChange("outerEye", value)}
+          >
+            <HStack spacing={4}>
+              <Radio value="circle">Circle</Radio>
+              <Radio value="square">Square</Radio>
+            </HStack>
+          </RadioGroup>
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Background Color</FormLabel>
+          <Input
+            type="color"
+            value={formData.backgroundColor}
+            onChange={(e) => handleChange("backgroundColor", e.target.value)}
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Size</FormLabel>
+          <Select
+            value={formData.size}
+            onChange={(e) => handleChange("size", e.target.value)}
+          >
+            <option value={200}>200</option>
+            <option value={400}>400</option>
+            <option value={800}>800</option>
+            <option value={1000}>1000</option>
+            <option value="custom">Custom</option>
+          </Select>
+        </FormControl>
+
+        {formData.size === "custom" && (
+          <FormControl>
+            <FormLabel>Custom Size</FormLabel>
+            <Input
+              type="number"
+              placeholder="Enter custom size"
+              value={formData.size}
+              onChange={(e) => handleChange("size", e.target.value)}
+            />
+          </FormControl>
+        )}
+
+        <FormControl>
+          <FormLabel>Output Format</FormLabel>
+          <Select
+            value={formData.outputFormat}
+            onChange={(e) => handleChange("outputFormat", e.target.value)}
+          >
+            <option value="svg">SVG</option>
+            <option value="jpg">JPG</option>
+            <option value="png">PNG</option>
+          </Select>
+        </FormControl>
+
+        <Button colorScheme="blue" onClick={handleSubmit}>
+          Generate QR Code
+        </Button>
+      </VStack>
+    </Box>
   );
 }
--- client/src/components/ImgComponent.js
+++ client/src/components/ImgComponent.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { Box, Image, Text } from "@chakra-ui/react";
 import "./ImgComponent.css";
-export default function ImgComponent({ imageUrl, caption }) {
+
+export default function ImgComponent({ imageUrl, caption = "sample" }) {
   return (
-    <Box className="imgBox" p={6} rounded={"md"}>
-      <Box textAlign="center" maxW="300px" mx="auto">
+    <Box
+      className="imgBox"
+      p={[3, 6]}
+      rounded={"md"}
+      maxW={["100%", "300px"]}
+      mx="auto"
+    >
+      <Box textAlign="center">
         <Image
           src="https://www.qrcode-monkey.com/img/qr/templates/ninja.svg"
           alt="Image"
           boxSize="100%"
           objectFit="cover"
           borderRadius="md"
-          rounded="lg"
+          rounded="xl"
         />
         {caption && (
-          <Text mt={2} color="gray.600" fontSize="sm">
+          <Text mt={2} color="gray.600" fontSize={["sm", "md"]}>
             {caption}
           </Text>
         )}
