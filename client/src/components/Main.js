5dffcf45a16cd87c0d74e33010cc9c65f1bf9f43
hrtechplus <40927446+hrtechplus@users.noreply.github.com>
2023-12-19 00:55:53 +0530

    Update Main.js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -4,27 +4,26 @@ import ImgComponent from "./ImgComponent";
 import Form from "./Form";
 import LinkBtn from "./LinkBtn";
 import "./Main.css";
-export default function CenteredBox() {
-  /////////////////
 
+export default function CenteredBox() {
   const [formData, setFormData] = useState({
     data: "",
-    imageUri: "icon://appstore",
-    innerEye: "circle",
-    outerEye: "circle",
     backgroundColor: "black",
     size: 200,
     format: "png",
+    ecc: "H",
+    qzone: "1",
+    margin: 19,
   });
 
-  const [qrCode, setQrCode] = useState(
-    "https://api.qrserver.com/v1/create-qr-code/?size=450x450&data=Example&ecc=H&color=392467&bgcolor=FFD1E3&margin=19&qzone=1"
-  );
+  const generateQRCodeUrl = () => {
+    const { color, size, data, ecc, backgroundColor, margin, qzone, format } =
+      formData;
+    return `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${data}&ecc=${ecc}&color=${color}&bgcolor=${backgroundColor}&margin=${margin}&qzone=${qzone}&format=${format}`;
+  };
 
   const handleInputChange = (e) => {
-    alert("hello");
     const { name, value } = e.target;
-    // e.target eken ganne  adala click una compoent eka
     setFormData({ ...formData, [name]: value });
   };
 
@@ -32,38 +31,35 @@ export default function CenteredBox() {
     setFormData({ ...formData, size: value });
   };
 
-  const handleAdvancedChange = (name, value) => {
-    setFormData({ ...formData, [name]: value });
-  };
-
   const handleFormSubmit = (e) => {
     e.preventDefault();
     // Add logic for handling form submission
     console.log(formData);
   };
-  ////////////////
+
+  const handleAdvancedChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
-    <Center minH="100vh" bg={"#e6e6e6"}>
+    <Center minH="100vh" bg="#e6e6e6">
       <Box
         className="MainContainer"
-        p={[2, 4, 8]} // Responsive padding
-        maxW={["100%", "400px", "600px"]} // Set maximum width for responsiveness
-        w="100%" // Take the full width
+        p={[2, 4, 8]}
+        maxW={["100%", "400px", "600px"]}
+        w="100%"
       >
         <Flex
           direction={["column", "column", "row"]}
           align={["center", "center", "flex-start"]}
           justify={["center", "center", "space-between"]}
-          gap={[4, 4, 8]} // Adjust the gap for spacing on smaller screens
+          gap={[4, 4, 8]}
         >
-          <Center
-            w={["100%", "100%", "40%"]} // Adjust the width for different screen sizes
-            mb={[4, 4, 0]} // Adjust margin-bottom for spacing on smaller screens
-          >
+          <Center w={["100%", "100%", "40%"]} mb={[4, 4, 0]}>
             <Flex direction="column">
-              <ImgComponent imageUrl={qrCode} />
+              <ImgComponent imageUrl={generateQRCodeUrl()} />
               <Center mt={4}>
-                <Flex maxW={"300px"} justify={"center"}>
+                <Flex maxW="300px" justify="center">
                   <LinkBtn />
                   <LinkBtn />
                   <LinkBtn />
@@ -71,16 +67,13 @@ export default function CenteredBox() {
               </Center>
             </Flex>
           </Center>
-          <Container
-            size={["100%", "150px", "150px"]} // Adjust the size for different screen sizes
-          >
+          <Container size={["100%", "150px", "150px"]}>
             <Form
               handleFormSubmit={handleFormSubmit}
               handleInputChange={handleInputChange}
               handleSliderChange={handleSliderChange}
               handleAdvancedChange={handleAdvancedChange}
               formData={formData}
-              setQrCode={setQrCode}
             />
           </Container>
         </Flex>
